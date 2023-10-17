331199fb9276cb1a738e6035a9bc74a22291f622
daniMB94 <dani_aliast@hotmail.com>
2023-10-17 18:49:05 +0200

    Conectando a bbdd

---
+++ tema4/prueba.php
@@ -0,0 +1,31 @@
+<?php
+
+    $dbh = null;
+
+    define('DB_NAME', 'servidor');
+    define('DB_USER', 'root');
+    define('DB_PASSWORD', 'toor');
+    define('DB_HOST', '172.17.0.2:3306'); //La IP del contenedor Mysql, y el puerto interno del contenedor
+
+    try {
+        $dbh = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME, DB_USER, DB_PASSWORD);
+        $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $dbh->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+    } catch (PDOException $e){
+        echo $e->getMessage();
+    }
+
+
+    $stmt = $dbh->prepare("SELECT * FROM usuarios");
+    // Especificamos el fetch mode antes de llamar a fetch()
+    $stmt->setFetchMode(PDO::FETCH_ASSOC);
+    // Ejecutamos
+    $stmt->execute();
+
+    while($row = $stmt->fetch()){
+        echo "Nombre: " . $row['nombre'] . "<br>";
+        echo "Email: " . $row['email'] . "<br>";
+    }
+    
+
+?>
