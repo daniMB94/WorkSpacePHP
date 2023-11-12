92dbdcef40e7b3ddddaf78cb9ed0d0ddb308808c
daniMB94 <dani_aliast@hotmail.com>
2023-11-12 21:43:16 +0100

    Agrego funcion signIn y comprueba existencia y comprobacion de password. Y muestra regalos de ese usuario

--- tema5/modelos/ModeloUsuarios.php
+++ tema5/modelos/ModeloUsuarios.php
@@ -36,6 +36,26 @@ class ModeloUsuarios{
         return $usuarios;
     }
 
+    public static function usuario($nickname){
+        $conexionObject = new ConexionBaseDeDatos();
+        $conexion = $conexionObject->getConexion();
+
+        $consulta = $conexion->prepare("SELECT * FROM Usuarios WHERE nickname = :nickname");
+        $consulta->bindParam(':nickname', $nickname);
+        $consulta->setFetchMode(PDO::FETCH_CLASS | PDO::FETCH_PROPS_LATE, 'regalosNavidad\modelos\Usuario'); //Nombre de la clase
+        $consulta-> execute();
+
+        $usuarios = $consulta->fetchAll();
+
+        $conexionObject->cerrarConexion();
+
+        foreach($usuarios as $usuario){
+            $user = new Usuario($usuario->getId(), $usuario->getNombre(), $usuario->getNickname(), $usuario->getPasswordC());
+        }
+
+        return $user;
+    }
+
     public static function confirmarExistenciaUsuario($nickname){
 
     
@@ -55,11 +75,8 @@ class ModeloUsuarios{
 
     public static function comprobarContraseniaUsuario($passwordC, $nickname) {
         if (self::confirmarExistenciaUsuario($nickname)){
-            $usuarios = self::todosLosUsuarios();
-            foreach($usuarios as $usuario){
-                if (strcmp($usuario->getPasswordC, $passwordC) == 0){
-                    return true;
-                }
+            if (strcmp(self::usuario($nickname)->getPasswordC(), $passwordC) == 0){
+                return true;
             }
         }
 
