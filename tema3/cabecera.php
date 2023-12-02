65a8b03e176aae00f0565792a2df0448193184ae
daniMB94 <dani_aliast@hotmail.com>
2023-12-02 18:08:00 +0100

    actualizacion

--- tema3/cabecera.php
+++ tema3/cabecera.php
@@ -26,12 +26,15 @@ session_start();
         <!-- Navbar Brand-->
         <a class="navbar-brand ps-3" href="index.html">Start Bootstrap</a>
         <!-- Sidebar Toggle-->
-        <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i class="fas fa-bars"></i></button>
+        <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i
+                class="fas fa-bars"></i></button>
         <!-- Navbar Search-->
         <form class="d-none d-md-inline-block form-inline ms-auto me-0 me-md-3 my-2 my-md-0">
             <div class="input-group">
-                <input class="form-control" type="text" placeholder="Search for..." aria-label="Search for..." aria-describedby="btnNavbarSearch" />
-                <button class="btn btn-primary" id="btnNavbarSearch" type="button"><i class="fas fa-search"></i></button>
+                <input class="form-control" type="text" placeholder="Search for..." aria-label="Search for..."
+                    aria-describedby="btnNavbarSearch" />
+                <button class="btn btn-primary" id="btnNavbarSearch" type="button"><i
+                        class="fas fa-search"></i></button>
             </div>
         </form>
         <!-- Navbar-->
@@ -43,9 +46,11 @@ session_start();
 
         ?>
 
+
         <ul class="navbar-nav ms-auto ms-md-0 me-3 me-lg-4">
             <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="fas fa-user fa-fw"></i></a>
+                <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown"
+                    aria-expanded="false"><i class="fas fa-user fa-fw"></i></a>
                 <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
                     <li><a class="dropdown-item" href="#!">Settings</a></li>
                     <li><a class="dropdown-item" href="#!">Activity Log</a></li>
@@ -70,19 +75,25 @@ session_start();
                     <div class="nav">
 
                         <div class="sb-sidenav-menu-heading">Acciones</div>
-                        <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseLayoutsProyectos" aria-expanded="false" aria-controls="collapseLayoutsProyectos">
+                        <a class="nav-link collapsed" href="#" data-bs-toggle="collapse"
+                            data-bs-target="#collapseLayoutsProyectos" aria-expanded="false"
+                            aria-controls="collapseLayoutsProyectos">
                             <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
                             Proyectos
                             <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
                         </a>
-                        <div class="collapse" id="collapseLayoutsProyectos" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
+                        <div class="collapse" id="collapseLayoutsProyectos" aria-labelledby="headingOne"
+                            data-bs-parent="#sidenavAccordion">
                             <nav class="sb-sidenav-menu-nested nav">
-                                <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseLayoutsAnadirProyecto" aria-expanded="false" aria-controls="collapseLayoutsAnadirProyecto">
+                                <a class="nav-link collapsed" href="#" data-bs-toggle="collapse"
+                                    data-bs-target="#collapseLayoutsAnadirProyecto" aria-expanded="false"
+                                    aria-controls="collapseLayoutsAnadirProyecto">
                                     <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
                                     Añadir Proyecto
                                     <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
                                 </a>
-                                <div class="collapse" id="collapseLayoutsAnadirProyecto" data-bs-parent="#collapseLayoutsProyectos">
+                                <div class="collapse" id="collapseLayoutsAnadirProyecto"
+                                    data-bs-parent="#collapseLayoutsProyectos">
                                     <nav class="sb-sidenav-menu-nested nav">
                                         <form action="">
                                             <input type="text">
